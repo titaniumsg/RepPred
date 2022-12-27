bb7da66f6a4a4521ba3696853059d2472b75eba9
Sagar Gupta <sagarg@sas.upenn.edu>
2022-12-26 19:33:23 -0500

    test

--- relax.py
+++ relax.py
@@ -46,11 +46,12 @@ def submit_to_cluster(command):
     with open("submit.sh", "w") as bashfile:
         bashfile.write("#!/bin/bash\n")
         bashfile.write("#SBATCH --time=2-23:59:59\n")
-        bashfile.write(". /etc/profile\n")
-        bashfile.write("module load OpenMPI/3.1.4-GCC-8.3.0\n")
-        bashfile.write("unset module\n")
+        # bashfile.write(". /etc/profile\n")
+        # bashfile.write("module load OpenMPI/3.1.4-GCC-8.3.0\n")
+        # bashfile.write("unset module\n")
 
-        bashfile.write(f"mpirun -n 3 {command}")
+        # bashfile.write(f"mpirun -n 3 {command}")
+        bashfile.write(f"{command}")
 
     run_command("sbatch submit.sh")
 
@@ -77,17 +78,16 @@ def relax(pdb_file, rosetta_dir, num_iterations=4):
     print(f"Relaxing {pdb_file} {num_iterations} time(s)\nLog file: {logfile}")
     # run_command(f"{rosetta_dir}/main/source/bin/relax.mpi.{extension} -database {rosetta_dir}/main/database -s {pdb_file} -nstruct {num_iterations} -out:path:score {output_dir} -out:path:pdb {output_dir} -score:weights ref2015_cart -relax:constrain_relax_to_start_coords -relax:cartesian -relax:ramp_constraints false -out:suffix _relaxed > {logfile}")
     submit_to_cluster(f"{rosetta_dir}/main/source/bin/relax.mpi.{extension} -database {rosetta_dir}/main/database -s {pdb_file} -nstruct {num_iterations} -out:path:score {output_dir} -out:path:pdb {output_dir} -score:weights ref2015_cart -relax:constrain_relax_to_start_coords -relax:cartesian -relax:ramp_constraints false -out:suffix _relaxed > {logfile}")
-    quit()
     
-    relaxed_threaded_total_score = {}
+    # relaxed_threaded_total_score = {}
     
-    for index in range(1, int(num_iterations)+1):
-        relaxed_threaded_filename = f"{pdb_file_no_extension}_relaxed_{str(index).zfill(4)}"
-        total_score = run_command(f"cat {output_dir}/score_relaxed.sc | grep {relaxed_threaded_filename}"+" | tail -1 | awk '{print $2}'")
-        if total_score == '': continue
-        total_score = float(total_score.strip())
-        relaxed_threaded_total_score[relaxed_threaded_filename] = total_score
+    # for index in range(1, int(num_iterations)+1):
+    #     relaxed_threaded_filename = f"{pdb_file_no_extension}_relaxed_{str(index).zfill(4)}"
+    #     total_score = run_command(f"cat {output_dir}/score_relaxed.sc | grep {relaxed_threaded_filename}"+" | tail -1 | awk '{print $2}'")
+    #     if total_score == '': continue
+    #     total_score = float(total_score.strip())
+    #     relaxed_threaded_total_score[relaxed_threaded_filename] = total_score
 
-    relaxed_threaded_best = min(relaxed_threaded_total_score, key=relaxed_threaded_total_score.get)
+    # relaxed_threaded_best = min(relaxed_threaded_total_score, key=relaxed_threaded_total_score.get)
 
-    return f"{output_dir}/{relaxed_threaded_best}.pdb", relaxed_threaded_best
+    # return f"{output_dir}/{relaxed_threaded_best}.pdb", relaxed_threaded_best
