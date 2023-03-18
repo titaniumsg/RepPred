0eb3f7f50c8d7b0fe34484d79f90dbdd69dd80b3
Sagar Gupta <sagarg@sas.upenn.edu>
2023-03-18 15:18:27 -0400

    v2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #       Sgourakis Lab
 #   Author: Sagar Gupta
-#   Date: November 29, 2022
+#   Date: March 18, 2023
 #   Email: sagarg@sas.upenn.edu
 
 # import required libraries
@@ -22,7 +22,7 @@ from scoring import scoring
 main.py is the entry point of RepPred
 
 This method is designed specifically to work with
-9mer peptides with anchor residues P2 and P9 bound to HLA-A*02:01
+9mer peptides with anchor residues P2 and P9 bound to an HLA
 
 '''
 
@@ -32,28 +32,39 @@ def get_args():
     Parse command line arguments
     '''
 
-    parser = argparse.ArgumentParser(description="Structural modeling of peptide/HLA-A*02:01 complexes")
-    parser.add_argument("-peptide_sequence", help="Sequence of desired peptide", required=True)
+    parser = argparse.ArgumentParser(description="Structural modeling of peptide/HLA complexes")
+    parser.add_argument("-seq_file", help="File containing HLA sequence (180 residues) followed by peptide sequence (9 residues) on a new line", required=True)
     parser.add_argument("-output_dir", help="Output directory (Highly recommend for this to be empty!)", default="example")
     parser.add_argument("-prefix", help="Prefix used for all files", default="test")
 
-    parser.add_argument("-num_iterations", help="Number of FastRelax rounds to run (at least 3 is recommended)", default="4", type=str)
+    parser.add_argument("-num_iters", help="Number of FastRelax rounds to run (at least 3 is recommended)", default="4", type=str)
 
     args = parser.parse_args()
 
     return args
 
-def main(peptide_sequence, output_dir, prefix, num_iterations):
+def main(seq_file, output_dir, prefix, num_iterations):
     
+    def read_seq_file(seq_file):
+        with open(seq_file, "r") as openfile:
+            sequences = openfile.readlines()
+            return sequences[0].strip(), sequences[1].strip()
+
+    hla_sequence, peptide_sequence = read_seq_file(seq_file)
+
+    assert len(peptide_sequence) == 9, f"Peptide must be length 9. A peptide of length {len(peptide_sequence)} is not supported"
+    assert len(hla_sequence) == 180, f"HLA must be length 180. An HLA of length {len(hla_sequence)} is not supported"
+    
+    print(f"Predicting the structure of the following peptide/HLA sequence:\n{hla_sequence}\n{peptide_sequence}\n")
+
     model = pickle.load(open(f'{REP_PRED_DIR}/regression/model.pkl', 'rb'))
     scaler = pickle.load(open(f'{REP_PRED_DIR}/regression/scaler.pkl', 'rb'))
     predicted_dscore_dict = {}
 
     structural_templates = sorted(glob.glob(f"{REP_PRED_DIR}/templates/*.pdb"))
     for template in tqdm(structural_templates):
-        if template != f'{REP_PRED_DIR}/templates/1A1M_reordered_const_cart_relaxed_0001.pdb': continue
         
-        threaded_filename = thread(template, f"{output_dir}/{prefix}", peptide_sequence, A0201_HLA_SEQUENCE)
+        threaded_filename = thread(template, f"{output_dir}/{prefix}", peptide_sequence, hla_sequence)
 
         relaxed_threaded_filename, relaxed_threaded_best = relax(threaded_filename, ROSETTA_DIR, num_iterations)
 
@@ -77,7 +88,7 @@ def main(peptide_sequence, output_dir, prefix, num_iterations):
             pdb_file_base = os.path.basename(template)
             writer.writerow([peptide_sequence, pdb_file_base, pred_dscore])
     
-    run_command(f"mkdir {output_dir}/workdir")
+    run_command(f"mkdir -p {output_dir}/workdir")
 
     run_command(f"rm {output_dir}/test.grishin")
 
@@ -90,10 +101,10 @@ def main(peptide_sequence, output_dir, prefix, num_iterations):
 
     run_command(f"cp {output_dir}/workdir/{best_model_base} {output_dir}")
     
-    print(f"The best model of {peptide_sequence}/HLA-A*02:01 has a predicted D-score of {predicted_dscore_dict[best_model]:.2f}\nThe modeled PDB file can be found at {output_dir}/{best_model_base}")
+    print(f"The best model of {output_dir} has a predicted D-score of {predicted_dscore_dict[best_model]:.2f}\nThe modeled PDB file can be found at {output_dir}/{best_model_base}")
 
 if __name__ == "__main__":
 
     args = get_args()
-    assert len(args.peptide_sequence) == 9, f"A peptide of length {len(args.peptide_sequence)} is not supported"
-    main(args.peptide_sequence, args.output_dir, args.prefix, args.num_iterations)
+    
+    main(args.seq_file, args.output_dir, args.prefix, args.num_iters)
